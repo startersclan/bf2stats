f32b730f221eca651b40873aa742df7867db8e18
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-01-22 06:51:26 +0000

    Fix (ASP): Log `php` errors to `stderr` when running in `docker`
    Previously, `ASP` `php` errors were logged to a file (`/src/ASP/system/logs/php_errors.log`). When running in docker, logging to a file can trip up users of `docker` who expect errors to be logged to `stderr`.
    
    Now, the env variable `PHP_VERSION` (present in official `php` images) is used to detect whether `ASP` is running in `docker`. If `ASP` is running in `docker`, `php` errors are logged to `stderr`. Otherwise, `php` errors are logged to a file.

--- src/ASP/index.php
+++ src/ASP/index.php
@@ -51,7 +51,10 @@
 */
     error_reporting(E_ALL);
     ini_set("log_errors", "1");
-    ini_set("error_log", SYSTEM_PATH . DS . 'logs' . DS . 'php_errors.log');
+    if (!getenv('PHP_VERSION')) {
+        # Not running in docker. Log errors to file
+        ini_set("error_log", SYSTEM_PATH . DS . 'logs' . DS . 'php_errors.log');
+    }
     ini_set("display_errors", "0");
 
 /*
