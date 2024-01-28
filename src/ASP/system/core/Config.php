0823af1f3366634182ed5fee9d6c9127e8a6e6bf
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2024-01-28 09:35:45 +0000

    Enhancement (ASP): Create config file if it does not exist
    Boolean values are now supported.

--- src/ASP/system/core/Config.php
+++ src/ASP/system/core/Config.php
@@ -28,13 +28,69 @@ class Config
 	{
         if(empty(self::$configFile))
         {
+			// Load config defaults
+			if ( !self::LoadDefault() ) {
+                throw new Exception('Failed to load default configuration!');
+			}
+			
             // Load the config File
             self::$configFile = SYSTEM_PATH . DS . 'config'. DS . 'config.php';
             if( !self::Load() )
             {
-                throw new Exception('Failed to load config file!');
+				// Create the config file
+				self::Save();
+				
+                // throw new Exception('Failed to load config file!');
             }
         }
+	}
+	
+/*
+| ---------------------------------------------------------------
+| Method: getDefault()
+| ---------------------------------------------------------------
+|
+| Returns the default configuration.
+|
+| @Return: array of the default configuration.
+|
+*/
+	private static function getDefault() 
+	{
+		$cfg = array(
+			'db_host' => '127.0.0.1',
+			'db_port' => 3306,
+			'db_name' => 'bf2stats',
+			'db_user' => 'admin',
+			'db_pass' => 'admin',
+			'admin_user' => 'admin',
+			'admin_pass' => 'admin',
+			'admin_hosts' => array('127.0.0.1','192.168.2.0/24','localhost','192.168.1.102','192.168.1.110','0.0.0.0'),
+			'admin_backup_path' => 'C:/wamp/www/ASP/system/database/backups/',
+			'admin_backup_ext' => '.bak',
+			'admin_ignore_ai' => 0,
+			'stats_ignore_ai' => 0,
+			'stats_min_game_time' => 0,
+			'stats_min_player_game_time' => 0,
+			'stats_players_min' => 1,
+			'stats_players_max' => 256,
+			'stats_rank_check' => 0,
+			'stats_rank_tenure' => 7,
+			'stats_process_smoc' => 1,
+			'stats_process_gen' => 1,
+			'stats_awds_complete' => 0,
+			'stats_lan_override' => '174.49.21.221',
+			'stats_local_pids' => array('LocalPlayer01','210.84.29.151','LocalPlayer02','210.84.29.151'),
+			'debug_lvl' => 2,
+			'game_hosts' => array('127.0.0.1','192.168.2.0/24','192.168.1.102','192.168.1.110','localhost','::1'),
+			'game_custom_mapid' => 700,
+			'game_unlocks' => 0,
+			'game_unlocks_bonus' => 2,
+			'game_unlocks_bonus_min' => 1,
+			'game_awds_ignore_time' => 0,
+			'game_default_pid' => 29000000,
+		);
+		return $cfg;
 	}
 	
 /*
@@ -131,6 +187,16 @@ class Config
 			if (is_numeric($val)) 
 			{
 				$cfg .= "\$$key = " . $val . ";\n";
+			}
+			
+			// If the value is boolean, then put in a "boolean" value
+			elseif (is_bool($val) || $val === 'true' || $val === 'false') 
+			{
+				if (!$val || $val === 'false') {
+					$cfg .= "\$$key = false;\n";
+				}else {
+					$cfg .= "\$$key = true;\n";
+				}
 			} 
 			
 			// Check for array values (admin_hosts, game_hosts, and stats_local_pids)
@@ -202,6 +268,11 @@ class Config
 		{
 			return false;
 		}
+	}
+	
+	protected static function LoadDefault() {
+		self::$data = self::getDefault();
+		return true;
 	}
 }
 
